d49d82f44bf563a47fc7c0af3ce422220c366618
Dylan Nguyen <dylannguyen@macbookpro.socal.rr.com>
2022-04-21 07:33:55 -0700

    created initial files, imported libraries

---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+import pyautogui, time
+from requests_html import HTMLSession
+import requests
+from lxml import html
+from urllib.parse import urljoin
+import pandas as pd
+
+
+
+
+
